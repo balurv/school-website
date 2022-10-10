f00d0a6dd25faa366125c10f389a3234330eee96
balajirv <ramini.venkatesh@gmail.com>
2022-10-10 10:25:09 +0530

    added validator dependency

--- src/main/java/com/eazybytes/eazyschool/controller/HolidayController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/HolidayController.java
@@ -4,6 +4,8 @@ import com.eazybytes.eazyschool.model.Holiday;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -13,11 +15,18 @@ import java.util.stream.Collectors;
 
 @Controller
 public class HolidayController {
-    @GetMapping("/holidays")
-    public String displayHolidays(@RequestParam(required = false)boolean festival,
-                                  @RequestParam(required = false)boolean federal,Model model){
-        model.addAttribute("festival", festival);
-        model.addAttribute("federal", federal);
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(@PathVariable String display, Model model){
+        if(null != display && display.equals("all")){
+            model.addAttribute("festival", true);
+            model.addAttribute("federal", true);
+        }
+        else if(null != display && display.equals("festival")){
+            model.addAttribute("festival", true);
+        }
+        else if(null != display && display.equals("federal")){
+            model.addAttribute("federal",true);
+        }
         List<Holiday> holidayList = Arrays.asList(
                 new Holiday("jan 1", "New year", Holiday.Type.FESTIVAL),
                 new Holiday("jan 14", "Sankranthi", Holiday.Type.FESTIVAL),
